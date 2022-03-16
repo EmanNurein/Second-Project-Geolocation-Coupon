46be6426dccbaa9fcc8f5eb7629faa078a37e650
EmanNurein <eamw96:gmail.com>
2022-03-16 02:32:44 +0200

    first file

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeMode(){
+    var mybody= document.body;
+    mybody.classList.toggle('mydark')
+}
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility ='visible';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility ='hidden';
+}
